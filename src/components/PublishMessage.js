ee4222e4f2a078048b677064dbaafecf0ea80cb4
Glendon Gengel <glendon.gengel@gmail.com>
2023-07-11 15:43:49 -0500

    Initial commit

---
+++ src/components/PublishMessage.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useAppContext } from './hooks';
+import { newMessage } from '../state/actions';
+
+function PublishMessage() {
+    const { state: { username }, pubsub: { publish } } = useAppContext();
+    const [text, setText] = useState('');
+  
+    const updateText = event => {
+      setText(event.target.value);
+    }
+  
+    const publishMessage = () => {
+      publish(newMessage({ text, username }));
+      console.log('publishing message ')
+    }
+  
+    const handleKeyPress = event => {
+      if (event.key === 'Enter') publishMessage();
+    }
+  
+    return (
+      <div>
+        <h3>Got something to say?</h3>
+        <input value={text} onChange={updateText} onKeyPress={handleKeyPress} />
+        {' '}
+        <button onClick={publishMessage}>Publish it!</button>
+      </div>
+    )
+  }
+  
+  export default PublishMessage;
